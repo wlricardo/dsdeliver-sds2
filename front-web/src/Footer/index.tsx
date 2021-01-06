e0d41074fc1224daadd51ada771a2ae4d9217ce0
wlricardo <wlricardo@gmail.com>
2021-01-06 18:31:15 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,26 @@
+import './styles.css';
+import { ReactComponent as YouTubeIcon } from './youtube.svg'
+import { ReactComponent as InstagramIcon } from './instagram.svg'
+import { ReactComponent as LinkedInIcon } from './linkedin.svg'
+
+
+function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior.
+            <div className="footer-icon">
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <YouTubeIcon/>
+                </a>
+                <a href="https://www.linkedin.com/school/devsuperior" target="_new">
+                    <LinkedInIcon/>
+                </a>
+                <a href="https://www.instagram.com/devsuperior.ig" target="_new">
+                    <InstagramIcon/>
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
