17020d1dda065c7589def6e1dc248a153bfcdf03
wlricardo <wlricardo@gmail.com>
2021-01-07 22:20:41 -0300

    Add Routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,17 +1,12 @@
-import { useState } from 'react';
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
+import Routes from './Routes';
 
 function App() {
-
   return (
     <>
-      <Navbar/>
-      <Home/>
+      <Routes />
     </>
-  )
-  
+  );  
 }
 
 export default App;
--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main-image.svg';
 import Footer from '../Footer';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
@@ -10,16 +11,16 @@ function Home() {
                     <div className="home-action">
                         <h1 className="home-title">
                             Faça seu pedido <br />
-                        que entregamos <br />
-                        pra você!!!
-                    </h1>
+                            que entregamos <br />
+                            pra você!!!
+                        </h1>
                         <h3 className="home-subtitle">
                             Escolha o seu pedido e em poucos minutos <br />
-                        levaremoss na sua porta
-                    </h3>
-                        <a href="orders" className="home-btn-order">
+                            levaremoss na sua porta
+                         </h3>
+                        <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
-                    </a>
+                        </Link>
                     </div>
                     <div className="home-image">
                         <MainImage />
