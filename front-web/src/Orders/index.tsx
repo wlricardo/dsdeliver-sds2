f648761a8ae8fd2ced7d9f098e41ed374543fe14
wlricardo <wlricardo@gmail.com>
2021-01-08 00:35:48 -0300

    Show Products dinamically from back-end

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,25 @@
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
+import { useEffect, useState } from 'react';
 import './styles.css';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader/>
+            <ProductsList products={products}/>
         </div>
     )
 }
